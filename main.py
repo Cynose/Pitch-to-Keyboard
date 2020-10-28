04b2450d25c0ea0d812d7e25e5ae8cbdaf59abda
Cynose <73034796+Cynose@users.noreply.github.com>
2020-10-28 16:21:06 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,51 @@
+import aubio
+import pyaudio
+import numpy as np 
+import keyboard
+
+#Initate Pyaudio 
+p = pyaudio.PyAudio()
+
+#stream thing
+stream = p.open(format=pyaudio.paFloat32,
+    channels=1, rate=20000, input=True,
+    frames_per_buffer=1024)
+
+#aubio thing
+pDetection = aubio.pitch(method = "default", buf_size = 2048,
+    hop_size = 2048//2, samplerate = 20000)
+
+
+# Set unit.
+pDetection.set_unit("Hz")
+pDetection.set_silence(-40)
+
+while True:
+
+    data = stream.read(1024)
+    samples = np.frombuffer(data,
+        dtype=aubio.float_type)
+    pitch = pDetection(samples)[0]
+
+    pitch = float(pitch)
+    print(str(pitch))
+
+    #KEYBOARD things
+
+    #These link certain pitches/notes to different keys 
+    
+    #E3
+    if pitch > 162 and pitch < 166:
+        keyboard.press('w')
+    #C3
+    elif pitch > 128 and pitch < 133:
+        keyboard.press('a')
+    #D3
+    elif pitch > 144 and pitch < 148:
+        keyboard.press('s')
+    #G3
+    elif pitch > 194 and pitch <198:
+        keyboard.press('d')
+    
+    
+  
